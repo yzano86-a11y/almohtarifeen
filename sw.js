9a10bfc35a0be13696285a06eae664e7a7bc668b
yzano86-a11y <yzano86@gmail.com>
2026-08-21 14:16:53 +0300

    Bust stale service worker cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='almohtarifeen-v1-5';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+const CACHE='almohtarifeen-v1-6';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./app.js','./style.css','./manifest.webmanifest']))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
   e.respondWith(fetch(e.request).then(r=>{
