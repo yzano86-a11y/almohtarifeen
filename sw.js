65c231e03bde32d427eea34dd40a9a87b55fd0da
yzano86-a11y <yzano86@gmail.com>
2026-08-26 16:02:39 +0300

    chore: remove old application runtime

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='almohtarifeen-v1-7-runtime';
-self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(fetch(event.request));
-});
