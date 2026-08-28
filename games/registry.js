143dab604c8628d9558e3558f8593ae86ce16e2c
yzano86-a11y <yzano86@gmail.com>
2026-08-28 22:51:47 +0300

    feat: register Al-Molook Bull game

--- games/registry.js
+++ games/registry.js
@@ -1,11 +1,13 @@
 import { startTarneeb } from './tarneeb/index.js';
 import { startBackgammon } from './backgammon/index.js';
 import { startCheckers } from './checkers/index.js';
+import { startBullEm } from './bullem/index.js';
 
 export const games = {
   tarneeb: { id:'tarneeb', name:'طرنيب', start:startTarneeb },
   backgammon: { id:'backgammon', name:'طاولة الزهر', start:startBackgammon },
-  checkers: { id:'checkers', name:'داما', start:startCheckers }
+  checkers: { id:'checkers', name:'داما', start:startCheckers },
+  bullem: { id:'bullem', name:'البلّ', start:startBullEm }
 };
 
 export function launchGame(id){
